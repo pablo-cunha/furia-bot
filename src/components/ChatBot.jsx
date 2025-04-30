a273738388c5829d4b02f9fbca3834a02d61f1b7
pablo-cunha <lcunha.pablo@gmail.com>
2025-04-30 18:10:33 -0300

    Atualiza tag link

--- src/components/ChatBot.jsx
+++ src/components/ChatBot.jsx
@@ -94,7 +94,9 @@ const ChatBot = () => {
                     <p>Desenvolvido por Pablo Cunha</p>
                 </div>
                 <div>
-                    <a href="https://github.com/pablo-cunha/furia-bot">Repositório no Github</a>
+                    <a href="https://github.com/pablo-cunha/furia-bot" target="_blank" rel="noopener noreferrer">
+                        Repositório no Github
+                    </a>
                 </div>        
             </div>
         </>
